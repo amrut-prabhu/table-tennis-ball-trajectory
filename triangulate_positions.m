86fc5682c18be5aac6a9ebb5454650e4de674749
amad-person <e0072565@u.nus.edu>
2018-11-12 20:04:09 +0800

    Perspective projection equations

--- triangulate_positions.m
+++ triangulate_positions.m
@@ -1,4 +1,4 @@
-% Triangulate 3D positions using Linear Least Squares Method
+% Triangulate 3D positions using SVD
 clear all;
 close all;
 
@@ -11,6 +11,12 @@ t1 = [1.3305621037591506e-01;
     -2.5319578738559911e-01;
     2.2444637695699150e+00];
 
+t1 = -inv(R1) * t1;
+
+K1 = [870.14531487461625 0 949.42001822880479;
+    0 870.14531487461625 487.20049852775117; 
+    0 0 1];
+
 % camera 2
 R2 = [9.4962278945631540e-01 3.1338395965783683e-01 -2.6554800661627576e-03; 
     1.1546856489995427e-01 -3.5774736713426591e-01 -9.2665194751235791e-01; 
@@ -19,6 +25,12 @@ R2 = [9.4962278945631540e-01 3.1338395965783683e-01 -2.6554800661627576e-03;
 t2 = [-4.2633372670025989e-02; 
     -3.5441906393933242e-01; 
     2.2750378317324982e+00];
+
+t2 = -inv(R2) * t2;
+
+K2 = [893.34367240024267 0 949.96816131377727;
+    0 893.34367240024267 594.79562177577259;
+    0 0 1];
 
 % camera 3
 R3 = [-9.9541881789113029e-01 3.8473906154401757e-02 -8.7527912881817604e-02;
@@ -29,10 +41,18 @@ t3 = [-6.0451734755080713e-02;
     -3.9533167111966377e-01; 
     2.2979640654841407e+00];
 
-for i = 0 : 9
+t3 = -inv(R3) * t3;
+
+K3 = [872.90852997159800 0 944.45161471037636;
+    0 872.90852997159800 564.47334036925656;
+    0 0 1];
+
+cam = plotCamera('Location', t1 ,'Orientation', R1,'Opacity', 0);
+
+for i = 0 : 0
     figH = figure;
     f = {strcat('CAM1-', num2str(i), '_cleaned.csv'), strcat('CAM2-', num2str(i), '_cleaned.csv'), strcat('CAM3-', num2str(i), '_cleaned.csv')};
-    res = triangulate_fn(f, R1, t1, R2, t2, R3, t3);
+    res = triangulate_fn(f, R1, t1, K1, R2, t2, K2, R3, t3, K3);
     disp(res);
     csvwrite(strcat('3dpts_', num2str(i), '.csv'), res);
     scatter3(res(:, 1), res(:, 2), res(:, 3));
@@ -41,12 +61,7 @@ end
 
 % Implement triangulation
 % Precondition: size(data1, 1) = size(data2, 1) = size(data3, 1)
-function points3D = triangulate_fn(f, R1, t1, R2, t2, R3, t3)
-    % Get camera pose(s)
-    P1 = get_camera_pose(R1, t1);
-    P2 = get_camera_pose(R2, t2);
-    P3 = get_camera_pose(R3, t3);
-    
+function points3D = triangulate_fn(f, R1, t1, K1, R2, t2, K2, R3, t3, K3)
     % Read data from csv
     points_1 = csvread(f{1}, 1, 4);
     points_2 = csvread(f{2}, 1, 4);
@@ -57,7 +72,7 @@ function points3D = triangulate_fn(f, R1, t1, R2, t2, R3, t3)
     points_2 = prepare_2d_pos(points_2);
     points_3 = prepare_2d_pos(points_3);
     
-    % Triagulate for each point
+    % Triangulate for each point
     npts = size(points_1, 1);
     points3D = zeros(npts, 3);
     
@@ -67,15 +82,29 @@ function points3D = triangulate_fn(f, R1, t1, R2, t2, R3, t3)
         pt_cam2 = points_2(i, :);
         pt_cam3 = points_3(i, :);
         
-        q1 = construct_mat(pt_cam1, P1);
-        q2 = construct_mat(pt_cam2, P2);
-        q3 = construct_mat(pt_cam3, P3);
+        [q1, k1] = construct_mat(pt_cam1, R1, t1, K1);
+        [q2, k2] = construct_mat(pt_cam2, R2, t2, K2);
+        [q3, k3] = construct_mat(pt_cam3, R3, t3, K3);
         
         Q = [q1; q2; q3];
         
         % Solving Q * res = zeros(6, 1), we get the solution
-        % Not sure if I am doing this part right
-        [A B V] = svd(Q);
+        disp(Q);
+        x = inv(transpose(Q) * Q) * transpose(Q) * [k1; k2; k3];
+        disp("LEAST SQUARES OVERDETERMINED");
+        disp(x);
+        
+        disp("MATLAB LINEAR LS");
+        x = lsqlin(Q, [k1; k2; k3]);
+        disp(x);
+        
+        disp("Q\c");
+        disp(Q\[k1; k2; k3]);
+        
+        disp("SVD");
+        Q = [q1 -k1; q2 -k2; q3 -k3];
+        [~, S, V] = svd(Q);
+        disp(S);
         res = V(:, size(V, 2));
         res = res / res(4);
         res = res(1 : 3);
@@ -84,19 +113,29 @@ function points3D = triangulate_fn(f, R1, t1, R2, t2, R3, t3)
     end
 end
 
-% Contruct matrix for given point
-function mat = construct_mat(pt, cam_mat)
-    mat = [0 pt(3) -pt(2); -pt(3) 0 pt(1); pt(2) -pt(1) 0;] * cam_mat;
+% Construct matrix for given point
+function [mat, k1k2] = construct_mat(pt, R, t, K)
+    fx = K(1, 1); % scalar
+    fy = K(2, 2); % scalar
+    If = R(1, :); % i row vector 
+    Jf = R(2, :); % j row vector
+    Kf = R(3, :); % k row vector
+    
+    x0 = K(1, 3) - 960; % scalar offset x
+    y0 = K(2, 3) - 540; % scalar offset y
+    
+    x = pt(1); % scalar
+    y = pt(2); % scalar
+    
+    A = fx * If - (x - x0) * Kf; % equation for x 
+    B = fy * Jf - (y - y0) * Kf; % equation for y
+    
+    k1k2 = [dot(A, t); dot(B, t)]; 
+    mat = [A; B];
 end
 
 % Prepares the 2D co-ordinates by adding a z column with value 1
 function points2D = prepare_2d_pos(p)
-    [r c] = size(p);
-    points2D = [p ones(r, 1)];
-end
-
-% Returns the camera pose for given values
-function P = get_camera_pose(R, t)
-    t = -inv(R) * t;
-    P = [R t];
+    [r, ~] = size(p);
+    points2D = [p(:, 1) - 960 p(:, 2) - 540 ones(r, 1)];
 end
